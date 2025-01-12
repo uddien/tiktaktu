8f059d5bb9051a0f17fb0bbdd5cc953a1d107917
uddien <azzammataram@gmail.com>
2025-01-12 22:17:27 +0800

    Inisialisasi logika

---
+++ tiktaktu.js
@@ -0,0 +1,113 @@
+"use strict";
+
+/*
+
+A SIMPLE TIC-TAC-TOE GAME IN JAVASCRIPT
+
+(1) Grid layout
+
+The game grid is represented in the array Grid.cells as follows:
+
+[0] [1] [2]
+[3] [4] [5]
+[6] [7] [8]
+
+The cells (array elements) hold the following numeric values:
+0 if not occupied, 1 for player, 3 for computer.
+This allows us to quickly get an overview of the game state:
+if the sum of all the cells in a row is 9, the computer wins,
+if it is 3 and all the cells are occupied, the human player wins,
+etc.
+
+(2) Strategy of makeComputerMove()
+
+The computer first  looks for almost completed rows, columns, and
+diagonals, where there are two fields occupied either by the human
+player or by the computer itself. If the computer can win by
+completing a sequence, it does so; if it can block the player from
+winning with the next move, it does that. If none of that applies,
+it plays the center field if that's free, otherwise it selects a
+random free field. This is not a 100 % certain strategy, but the
+gameplay experience is fairly decent.
+
+*/
+
+//==================================
+// EVENT BINDINGS
+//==================================
+
+// Bind Esc key to closing the modal dialog
+document.onkeypress = function (evt) {
+    evt = evt || window.event;
+    var modal = document.getElementsByClassName("modal")[0];
+    if (evt.keyCode === 27) {
+        modal.style.display = "none";
+    }
+};
+
+// When the user clicks anywhere outside of the modal dialog, close it
+window.onclick = function (evt) {
+    var modal = document.getElementsByClassName("modal")[0];
+    if (evt.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
+//==================================
+// HELPER FUNCTIONS
+//==================================
+function sumArray(array) {
+    var sum = 0,
+        i = 0;
+    for (i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+function isInArray(element, array) {
+    if (array.indexOf(element) > -1) {
+        return true;
+    }
+    return false;
+}
+
+function shuffleArray(array) {
+    var counter = array.length,
+        temp,
+        index;
+    while (counter > 0) {
+        index = Math.floor(Math.random() * counter);
+        counter--;
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
+
+function intRandom(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+// GLOBAL VARIABLES
+var moves = 0,
+    winner = 0,
+    x = 1,
+    o = 3,
+    player = x,
+    computer = o,
+    whoseTurn = x,
+    gameOver = false,
+    score = {
+        ties: 0,
+        player: 0,
+        computer: 0
+    },
+    xText = "<span class=\"x\">&times;</class>",
+    oText = "<span class=\"o\">o</class>",
+    playerText = xText,
+    computerText = oText,
+    difficulty = 1,
+    myGrid = null;
